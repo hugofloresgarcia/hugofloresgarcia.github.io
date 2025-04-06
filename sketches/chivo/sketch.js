2b41e70195c381ab9da918a2c6df0618f35ae955
hugofloresgarcia <hf01049@georgiasouthern.edu>
2025-04-06 15:38:16 -0500

    chivo

---
+++ sketches/chivo/sketch.js
@@ -0,0 +1,67 @@
+// Click and drag the mouse to view the scene from different angles.
+
+let shape;
+let bgColor;
+
+// Load the file and create a p5.Geometry object.
+function preload() {
+  shape = loadModel('/assets/chivo.obj', true);
+}
+
+// let WIDTH = 
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+
+  describe('A white teapot drawn against a gray background.');
+
+  bgColor = color("#5B2027");
+}
+
+
+
+function color_vary(clr, amt) {
+  // vary hsb by amt
+  let h = hue(clr);
+  let s = saturation(clr);
+  let b = brightness(clr);
+  return color(
+    h + random(-amt, amt), 
+    s + random(-amt, amt), 
+    b + random(-amt, amt)
+  );
+}
+
+// on click on the canvas, redirect to cabezadechivo.com
+function mousePressed() {
+  window.open("https://cabezadechivo.com", "_blank");
+}
+
+function draw() {
+  // background("#5B2027");
+  // slight variation on the background color
+  // bgColor.setAlpha(255);
+
+  // bgColor = color_vary(bgColor, 10);
+
+  // vary color only every 10 frames
+  if (frameCount % 10 == 0) {
+    bgColor = color_vary(bgColor, 10);
+  }
+  
+  background(bgColor);
+
+
+  // Enable orbiting with the mouse.
+  orbitControl();
+
+  // noStroke();
+  rotateY(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+
+  // zoom in a bit
+  scale(2);
+
+  model(shape);
+
+}
