64d3eaeb9f8c746cfb050830b6222ed4bc291efd
hugo flores <huferflo@gmail.com>
2025-09-20 09:22:44 -0500

    today

--- sketches/hello/sketch.js
+++ sketches/hello/sketch.js
@@ -1,5 +1,11 @@
 let environment = {};
 let WIDTH, HEIGHT;
+let numMsgs;
+
+let bgColor;
+let textColors = [];
+let textColors2 = [];
+var frameCount = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -8,6 +14,15 @@ function setup() {
   colorMode(HSB);
   textAlign(CENTER, CENTER);
   textSize(12);
+
+  numMsgs = int(HEIGHT / 40);
+  // bgColor = color(random(360), 80, 120);
+  bgColor = color(0, 0, 0);
+
+  for (i = 0; i < numMsgs; i++) {
+    textColors.push(color(random(360), 80, 120));
+    textColors2.push(textColors[i]);
+  }
 
   environment.flowers = [];
   environment.transforms = [];
@@ -39,48 +54,61 @@ function setup() {
   }
 }
 
-var frameCount = 0;
+
+
+
 function draw() {
-  background(120, 20, 95); // pale green
+  background(bgColor); // pale green
   frameCount = (frameCount + 1) % 10000;
 
-  for (let i = 0; i < environment.flowers.length; i++) {
-    let flower = environment.flowers[i];
-    let tfm = environment.transforms[i];
-
-    // Subtle animation
-    let bounce = sin(frameCount * tfm.freq + i) * 5;
-    let x = flower.center.x + tfm.dir * sin(frameCount * 0.01 + i) * 3;
-    let y = flower.center.y + bounce;
-
-    // Draw rotating flower
-    push();
-    translate(x, y);
-    rotate(frameCount * tfm.rotSpeed * tfm.dir);
-    noStroke();
-    fill(flower.clr);
-    ellipse(0, 0, 20, 20); // center ellipse
-    pop();
-
-    // Draw label text slightly above
-    fill(0);
-    text("hello cami!", x, y - 18); // slightly above flower
+  if (frameCount % 120 === 0) {
+    // textColors.();
+    for (i = 0; i < numMsgs-1; i++) {
+      textColors[i] = textColors[i + 1];
+    }
+    textColors[numMsgs-1] = color(random(360), 120, 80)
   }
 
+  // for (let i = 0; i < environment.flowers.length; i++) {
+  //   let flower = environment.flowers[i];
+  //   let tfm = environment.transforms[i];
+
+  //   // Subtle animation
+  //   let bounce = sin(frameCount * tfm.freq + i) * 5;
+  //   let x = flower.center.x + tfm.dir * sin(frameCount * 0.01 + i) * 3;
+  //   let y = flower.center.y + bounce;
+
+  //   // Draw rotating flower
+  //   push();
+  //   translate(x, y);
+  //   rotate(frameCount * tfm.rotSpeed * tfm.dir);
+  //   noStroke();
+  //   fill(flower.clr);
+  //   ellipse(0, 0, 20, 20); // center ellipse
+  //   pop();
+
+  //   // Draw label text slightly above
+  //   fill(0);
+  //   text("hello cami!", x, y - 18); // slightly above flower
+  // }
+  
+
   // Main header message
-  textSize(24);
-  let today = new Date();
-  let dateStr = today.toDateString();
-  fill(0);
-  // make a background rectangle for text
-  var msg = `hola cami! :) today is ${dateStr}`;
-  let textW = textWidth(msg) + 10;
-  let textH = 30;
-  fill(120, 80, 85);
-  rectMode(CENTER);
-  rect(WIDTH / 2, 40, textW, textH, 5);
-  fill(255);
-  text(msg, WIDTH / 2, 40);
+  for (i = 0; i < numMsgs; i++){
+    textSize(24);
+    let today = new Date();
+    let dateStr = today.toDateString();
+    fill(0);
+    // make a background rectangle for text
+    var msg = `hola cami!! <3 have a good flight today!! -h`;
+    let textW = textWidth(msg) + 10;
+    let textH = 30;
+    fill(textColors[i]);
+    // rectMode(CENTER);
+    // rect(WIDTH / 2, 40 * i, textW, textH, 5);
+    // fill(255);
+    text(msg, WIDTH / 2, 40 * i - (frameCount % 40));
+  }
 }
 
 function windowResized() {
